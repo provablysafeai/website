0f182c0942f6d5bebf3886ab69a086c75067edd8
Helder S Ribeiro <helder@agentofuser.com>
2024-03-09 00:01:49 -0400

    update head and footer

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import type { FC, ReactNode } from 'react'
 import { useRouter } from 'next/router'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { DocsThemeConfig, LocaleSwitch, useConfig } from 'nextra-theme-docs'
+import cn from 'clsx'
+
+export function renderComponent<T>(
+  ComponentOrNode: FC<T> | ReactNode,
+  props?: T
+) {
+  if (!ComponentOrNode) return null
+  if (typeof ComponentOrNode !== 'function') return ComponentOrNode
+  return <ComponentOrNode {...props} />
+}
 
 const config: DocsThemeConfig = {
   logo: <span>Provably Safe AI</span>,
@@ -10,10 +22,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.provablysafe.ai',
   },
-  docsRepositoryBase: 'https://github.com/provablysafeai/website',
-  footer: {
-    text: 'Provably Safe AI',
-  },
+  docsRepositoryBase: 'https://github.com/provablysafeai/website/tree/main',
   useNextSeoProps() {
     const { asPath } = useRouter()
     if (asPath !== '/') {
@@ -21,6 +30,56 @@ const config: DocsThemeConfig = {
         titleTemplate: '%s – Provably Safe AI',
       }
     }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://provablysafe.ai' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || 'Provably Safe AI'}
+        />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Formal Methods for AI Safety'}
+        />
+      </>
+    )
+  },
+  navigation: {
+    prev: true,
+    next: true,
+  },
+  feedback: {
+    content: 'Give feedback',
+    useLink: () => 'https://github.com/provablysafeai/website/issues/new',
+  },
+  footer: {
+    text: 'Provably Safe AI',
+    component: () => {
+      const config = useConfig()
+      return (
+        <>
+          <footer className="nx-bg-gray-100 nx-pb-[env(safe-area-inset-bottom)] dark:nx-bg-neutral-900 print:nx-bg-transparent">
+            <div
+              className={cn(
+                'nx-mx-auto nx-flex nx-max-w-[90rem] nx-justify-center nx-py-12 nx-text-gray-600 dark:nx-text-gray-400 md:nx-justify-start',
+                'nx-pl-[max(env(safe-area-inset-left),1.5rem)] nx-pr-[max(env(safe-area-inset-right),1.5rem)]'
+              )}
+            >
+              {renderComponent(config.footer.text)}
+            </div>
+          </footer>
+          <SpeedInsights />
+        </>
+      )
+    },
   },
 }
 

c302b311c208848961ec0f9c83b561bea94fa8fc
Helder S Ribeiro <helder@agentofuser.com>
2024-03-15 18:22:55 -0400

    set up redirects for newsletter

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  async redirects() {
+    return [
+      {
+        source: '/p/:slug*',
+        destination: 'https://news.provablysafe.ai/p/:slug*',
+        basePath: false,
+        permanent: true,
+      },
+      {
+        source: '/archive',
+        destination: 'https://news.provablysafe.ai/archive',
+        basePath: false,
+        permanent: true,
+      },
+    ]
+  },
+})
